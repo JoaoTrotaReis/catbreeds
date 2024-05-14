6669008846fc239f286a13b1b0328b91601ac0aa
João Reis <joaotrotareis@gmail.com>
2024-05-14 15:07:22 +0100

    Added new unit tests to validate the toggle favorite functionality on CatBreedListViewModel

--- app/src/test/java/com/joaoreis/catbreeds/catbreedlist/presentation/CatBreedListViewModelTests.kt
+++ app/src/test/java/com/joaoreis/catbreeds/catbreedlist/presentation/CatBreedListViewModelTests.kt
@@ -99,4 +99,70 @@ class CatBreedListViewModelTests {
             assertEquals(expectedViewState, awaitItem())
         }
     }
+
+    @Test
+    fun `Given the cat breed list is loaded When a cat breed is added to favorites Then emit a new loaded view state with that cat breed as favorite`() = runTest {
+        val breedListInteractor = FakeBreedListInteractor(
+            currentState = BreedListState.Loaded(
+                listOf(
+                    CatBreed("id1", "name1", "image1", "origin1", "description1", listOf("temperament"), false),
+                    CatBreed("id2", "name2", "image2", "origin2", "description2", listOf("temperament"), false)
+                )
+            )
+        )
+
+        val favoriteListInteractor = FakeFavoriteCatBreedsInteractor(
+            currentState = FavoriteCatBreedsState.Idle
+        )
+
+        val expectedViewState = CatBreedListViewState.Loaded(
+            listOf(
+                CatBreedViewItem("id1","image1", "name1", true),
+                CatBreedViewItem("id2","image2", "name2", false)
+            )
+        )
+
+        val viewModel = CatBreedListViewModel(
+            breedListInteractor, favoriteListInteractor
+        )
+
+        viewModel.viewState.test {
+            awaitItem()
+            viewModel.toggleFavorite("id1", true)
+            assertEquals(expectedViewState, awaitItem())
+        }
+    }
+
+    @Test
+    fun `Given the cat breed list is loaded When a cat breed is removed from the favorites Then emit a new loaded view state with that cat breed as not favorite`() = runTest {
+        val breedListInteractor = FakeBreedListInteractor(
+            currentState = BreedListState.Loaded(
+                listOf(
+                    CatBreed("id1", "name1", "image1", "origin1", "description1", listOf("temperament"), true),
+                    CatBreed("id2", "name2", "image2", "origin2", "description2", listOf("temperament"), false)
+                )
+            )
+        )
+
+        val favoriteListInteractor = FakeFavoriteCatBreedsInteractor(
+            currentState = FavoriteCatBreedsState.Idle
+        )
+
+        val expectedViewState = CatBreedListViewState.Loaded(
+            listOf(
+                CatBreedViewItem("id1","image1", "name1", false),
+                CatBreedViewItem("id2","image2", "name2", false)
+            )
+        )
+
+        val viewModel = CatBreedListViewModel(
+            breedListInteractor, favoriteListInteractor
+        )
+
+        viewModel.viewState.test {
+            awaitItem()
+            viewModel.toggleFavorite("id1", false)
+            assertEquals(expectedViewState, awaitItem())
+        }
+    }
 }
